63b66475a6ce6e47a1255ba6bcfbd0d79de03aac
FabMaster2000 <68748739+FabMaster2000@users.noreply.github.com>
2021-10-20 11:24:14 +0200

    feature:Token:Create Token, validate/invalidate Token, Token Testing
    Renamed variables, create URL via new function

--- pkg/token/token.go
+++ pkg/token/token.go
@@ -1,19 +1,41 @@
 package token
 
-var TokenMap map[string] string
+import (
+	"math/rand"
+)
+
+const letterBytes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 type Token string
 
-type Validator func(t Token) (bool, string)
+var tokenMap = map[Token]string{}
 
-func Validate(t Token) (bool, string) {
-	return true, "lol"
+func VerifyToken(token Token) bool {
+	if _, ok := tokenMap[token]; ok {
+		return true
+	} else {
+		return false
+	}
 }
 
-func CreateToken(location string)  map[string] string{
+func CreateToken(location string) Token {
+	b := make([]byte, 8)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
 
+	if len(tokenMap) == 2{
+		deleteFirstToken()
+	}
+
+	newToken := Token(b)
+	tokenMap[newToken] = location
+	return newToken
 }
 
-func (Token) getMap() map[string] string{
-	return TokenMap
+func deleteFirstToken() {
+	for k, _ := range tokenMap {
+		delete(tokenMap, k)
+		break
+	}
 }
--- pkg/token/token_test.go
+++ pkg/token/token_test.go
@@ -6,10 +6,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestValidateToken(t *testing.T) {
-	tok := Token("test")
+func TestVerifyToken(t *testing.T) {
+	tok1 := Token("test")
+	tok2 := CreateToken("DE")
+	tok3 := CreateToken("ENG")
 
-	valid, _ := Validate(tok)
+	assert.False(t, VerifyToken(tok1))
+	assert.True(t, VerifyToken(tok2))
+	assert.True(t, VerifyToken(tok3))
 
-	assert.True(t, valid)
+	//create Token to invalidate tok2
+	tok4 := CreateToken("IT")
+
+	assert.False(t, VerifyToken(tok2))
+	assert.True(t, VerifyToken(tok3))
+	assert.True(t, VerifyToken(tok4))
 }
