c5769196eb4f8ae9cd85253de8b07a13113c0e5f
FabMaster2000 <68748739+FabMaster2000@users.noreply.github.com>
2021-10-20 08:39:43 +0200

    Update files

--- pkg/token/token.go
+++ pkg/token/token.go
@@ -1,7 +1,17 @@
 package token
 
+var TokenMap map[string] string
+
 type Token string
 
 func VerifyToken(token Token) bool {
 	return true
+}
+
+func CreateToken(location string)  map[string] string{
+
+}
+
+func (Token) getMap() map[string] string{
+	return TokenMap
 }
