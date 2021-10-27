e18fe3746d247f0408791fe697e967b70c9e1fb0
FabMaster2000 <68748739+FabMaster2000@users.noreply.github.com>
2021-10-27 17:15:33 +0200

    QR-Website and Test
    Foreach Location an own website, Qr-Website changed, Validate Location, TokenMap is new structured, New Tests added and costumized

--- cmd/qr-web/main.go
+++ cmd/qr-web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"DHBW_Golang_Project/pkg/location"
 	"DHBW_Golang_Project/pkg/token"
 	"flag"
 	"fmt"
@@ -8,78 +9,133 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"path"
 	"time"
 )
-
-var templateQR = template.Must(
-	template.New("qrCode").Parse(`<!DOCTYPE html>
-		<html lang="en">
-			<head>
-    			<meta charset="UTF-8">
-    			<meta http-equiv="refresh" content="{{.RefreshTime}}">
-			</head>
-			<body>
-				<img src="/pic/qr_code.jpg" alt="QR-Code" width="256" height="256">
-			</body>
-		</html>`))
 
 type StartVariableStruct struct {
 	RefreshTime int
 	Port        string
 }
 
+type htmlVariableStruct struct {
+	RefreshTime int
+	Location string
+}
+
 var CheckinUrl string
+
+var actualLocation location.Location
+
+var qrTemplate *template.Template
+
+var pathToQr string
+
+var pathToLocation string
 
 var StartVariable StartVariableStruct
 
+var htmlVariable htmlVariableStruct
+
 func main() {
+
 	flag.Parse()
+
+	parseTemplates("web/templates")
+	pathToPic("assets/qr-codes/")
+	pathToLocations("assets/")
+
 	go reloadQR()
 
-	if err := http.ListenAndServeTLS(":"+StartVariable.Port, "../../assets/ssl/server.crt", "../../assets/ssl/server.key", qrMux()); err != nil {
-		log.Fatal(err)
-	}
+	err := http.ListenAndServeTLS(":"+StartVariable.Port, "./assets/ssl/server.crt", "./assets/ssl/server.key", qrMux())
+
+	checkError(err)
+}
+
+func setLocation(loc location.Location) {
+	actualLocation = loc
+}
+
+func parseTemplates(templateDir string) {
+	qrTemplate = template.Must(template.ParseFiles(path.Join(templateDir, "qr-code.html")))
 }
 
 func init() {
 	var rT int
 	var p string
-	flag.IntVar(&rT, "refreshTime", 60, "Start variable for refresh time.")
+	flag.IntVar(&rT, "refreshTime", 2, "Start variable for refresh time.")
 	flag.StringVar(&p, "port", "8142", "Start variable for URL-Link")
 	StartVariable = StartVariableStruct{RefreshTime: rT, Port: p}
+	htmlVariable = htmlVariableStruct{RefreshTime: rT}
 }
 
 func reloadQR() {
 	createUrl()
 	ticker := time.NewTicker(time.Duration(StartVariable.RefreshTime * 1000000000))
-	for _ = range ticker.C {
+	for range ticker.C {
 		createUrl()
 	}
 }
 
 func createUrl() {
-	CheckinUrl = "localhost:" + StartVariable.Port + "?token=" + fmt.Sprint(token.CreateToken("DE"))
-	err := qrcode.WriteFile(CheckinUrl, qrcode.Medium, 256, "pic/qr_code.jpg")
+	locations,_ := location.ReadLocations(pathToLocation + "locations.xml")
+	for _, loc := range locations{
+		CheckinUrl = "localhost:" + StartVariable.Port + "?token=" + string(token.CreateToken(loc))
 
-	if err != nil {
-		log.Fatal(err)
+		err := qrcode.WriteFile(CheckinUrl, qrcode.Medium, 256, pathToQr + string(loc)+".jpg")
+
+		checkError(err)
 	}
 }
 
-func handleQR(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("../../web/templates/qr-code.html"))
-
-	if err := tmpl.Execute(w, StartVariable); err != nil {
-		log.Fatal(err)
+func checkError(err error) bool {
+	if err != nil{
+		log.Println(err)
+		return false
+	}else{
+		return true
 	}
+}
+
+func pathToPic(pictureDir string){
+	pathToQr = pictureDir
+}
+
+func pathToLocations(locationsDir string){
+	pathToLocation = locationsDir
+}
+
+func handleQR(w http.ResponseWriter, r *http.Request) {
+	actualLocation = location.Location(r.URL.Query()["location"][0])
+
+	if valideLocation(actualLocation) {
+		htmlVariable.Location = string(actualLocation)
+
+		err := qrTemplate.Execute(w, htmlVariable)
+		checkError(err)
+	}
+}
+
+
+
+func valideLocation(expLocations location.Location) bool{
+	locations,_ := location.ReadLocations(pathToLocation + "locations.xml")
+	for _, actLocation := range locations{
+		if actLocation == expLocations{
+			return true
+		}
+	}
+	return false
 }
 
 func qrMux() http.Handler {
 	mux := http.NewServeMux()
-	mux.Handle("/pic/", http.StripPrefix("/pic/", http.FileServer(http.Dir("./pic"))))
+	mux.Handle("/qr-codes/", http.StripPrefix("/qr-codes/", http.FileServer(http.Dir("assets/qr-codes/"))))
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
 
-	mux.HandleFunc("/", handleQR)
+	mux.HandleFunc("/qr", handleQR)
 	fmt.Printf("Starting server at port %v\n", StartVariable.Port)
+	fmt.Printf("Refresh time: %v\n", StartVariable.RefreshTime)
 
 	return mux
 }
--- pkg/token/token.go
+++ pkg/token/token.go
@@ -1,40 +1,35 @@
 package token
 
 import (
+	"DHBW_Golang_Project/pkg/location"
 	"math/rand"
 )
 
 const letterBytes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 type Token string
 type Validator func(t Token) (bool, string)
-var tokenMap = map[Token]string{}
+var tokenMap = map[location.Location][]Token{}
 
-func Validate(token Token) (bool, string) {
-	if location, ok := tokenMap[token]; ok {
-		return true, location
-	} else {
-		return false, ""
+func Validate(expToken Token, tokenLocation location.Location) bool {
+	for _, actToken := range tokenMap[tokenLocation] {
+		if actToken == expToken {
+			return true
+		}
 	}
+	return false
 }
 
-func CreateToken(location string) Token {
+func CreateToken(tokenLocation location.Location) Token {
 	b := make([]byte, 8)
 	for i := range b {
 		b[i] = letterBytes[rand.Intn(len(letterBytes))]
 	}
 
-	if len(tokenMap) == 2 {
-		deleteFirstToken()
+	if len(tokenMap[tokenLocation]) == 2 {
+		tokenMap[tokenLocation] = []Token {tokenMap[tokenLocation][1]}
 	}
 
 	newToken := Token(b)
-	tokenMap[newToken] = location
+	tokenMap[tokenLocation] = append(tokenMap[tokenLocation], newToken)
 	return newToken
-}
-
-func deleteFirstToken() {
-	for k := range tokenMap {
-		delete(tokenMap, k)
-		break
-	}
 }
