9a97cd4277490a9474f96a7e2caed8c9fc8427de
Niel Rohling <niel_samuel.rohling@mail.schwarz>
2021-11-06 17:48:49 +0100

    waitegroup, new certificate

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,8 +5,12 @@ import (
 )
 
 var (
+	Hostname    *string
 	CheckinPort *int
 	QRCodePort  *int
+
+	CertificateFilePath *string
+	KeyFilePath         *string
 
 	RefreshTime    *int
 	CookieLifetime *int
@@ -20,15 +24,19 @@ var (
 
 func Configure() {
 	if !parsed {
+		Hostname = flag.String("hostname", "localhost", "hostname of application")
 		CheckinPort = flag.Int("checkinPort", 8443, "port of checkin server")
 		QRCodePort = flag.Int("qrCodePort", 8444, "port of qr-code server")
+
+		CertificateFilePath = flag.String("certificateFile", "assets/ssl/cert.pem", "path to ssl certificate")
+		KeyFilePath = flag.String("keyFile", "assets/ssl/key.pem", "path to ssl key")
 
 		RefreshTime = flag.Int("refreshTime", 60, "refresh time for qr-codes in seconds")
 		CookieLifetime = flag.Int("cookieLifeTime", 24*356, "Lifetime of Cookies in Hours")
 
 		TemplatePath = flag.String("templatePath", "web/templates", "path to html-template directory")
 		QrCodePath = flag.String("qrCodePath", "assets/qr-codes", "path to save qr-codes")
-		LocationFilePath = flag.String("locationFilePath", "assets/locations.xml", "path to xml file with locations")
+		LocationFilePath = flag.String("locationFile", "assets/locations.xml", "path to xml file with locations")
 	}
 	flag.Parse()
 	parsed = true
