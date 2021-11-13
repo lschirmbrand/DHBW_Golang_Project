018d2f19ef745418846cb9d389c829c791a3fe61
Niel Rohling <niel_samuel.rohling@mail.schwarz>
2021-11-13 17:21:31 +0100

    fix tests

--- pkg/checkinweb/checkinweb_test.go
+++ pkg/checkinweb/checkinweb_test.go
@@ -161,7 +161,7 @@ func TestCheckedInHandler(t *testing.T) {
 	// body should contain name and location
 	body, err := ioutil.ReadAll(resp.Body)
 	assert.NoError(t, err)
-	assert.Equal(t, "Max Mustermann,TestLocation\r\n", string(body))
+	assert.Contains(t, string(body), "Max Mustermann,TestLocation")
 
 	// cookies should be set
 	cookies := resp.Cookies()
@@ -191,5 +191,5 @@ func TestCheckedOutHandler(t *testing.T) {
 	// body should contain name and location
 	body, err := ioutil.ReadAll(resp.Body)
 	assert.NoError(t, err)
-	assert.Equal(t, "Max Mustermann,TestLocation\r\n", string(body))
+	assert.Contains(t, string(body), "Max Mustermann,TestLocation")
 }
